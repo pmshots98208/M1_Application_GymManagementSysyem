dc94cc5a363e28efb7699295c7271f38744b1798
Priyanshu  Mishra <94250186+pmshots98208@users.noreply.github.com>
2021-11-22 06:08:39 -0800

    Update workflow.c
    C file is updated.

--- .github/workflows/workflow.c
+++ .github/workflows/workflow.c
@@ -1,1 +1,29 @@
+#include <stdio.h>
 
+void main()
+
+{    
+
+       float num; 
+
+       clrscr();
+
+       do
+
+        {
+
+                 printf(“Enter a non-negative number: “);
+
+                 scanf (“%f”, &num);
+
+                 if (num < 0)
+
+                    printf(“Error: Invalid data.\n”);
+
+        }        while (num < 0);
+
+                  printf(“Number: %f Square root: %f \n”, num, sqrt(num));
+
+                        getch();
+
+}
