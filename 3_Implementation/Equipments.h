f1074316339e6219a392e10df14a3e39e0a84cd2
Priyanshu  Mishra <94250186+pmshots98208@users.noreply.github.com>
2021-11-23 09:41:12 -0800

    Delete Equipments.h

--- 3_Implementation/Equipments.h
+++
@@ -1,7 +0,0 @@
-#include <windows.h>
-
-void Equipments()
-{  
-   ShellExecute(NULL, "open", "https://www.gymventures.com/gym-equipment-names-and-pictures/#barbellsgymequipment",
-                NULL, NULL, SW_SHOWNORMAL);
-}
